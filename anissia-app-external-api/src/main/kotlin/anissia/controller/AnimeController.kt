d5b3c4aefac9fb9d654508de12ca9ef4d0f88e00
PARK Yong Seo <j@saro.me>
2020-12-18 21:01:53 +0900

    create AnimeController.kt

---
+++ anissia-app-external-api/src/main/kotlin/anissia/controller/AnimeController.kt
@@ -0,0 +1,10 @@
+package anissia.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/api/anime")
+class AnimeController {
+
+}
