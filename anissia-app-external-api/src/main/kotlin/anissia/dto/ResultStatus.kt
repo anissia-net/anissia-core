d66b64b64fb11369a3ccac7d46af5691909d9850
PARK Yong Seo <j@saro.me>
2020-12-19 20:05:53 +0900

    basic result set

---
+++ anissia-app-external-api/src/main/kotlin/anissia/dto/ResultStatus.kt
@@ -0,0 +1,11 @@
+package anissia.dto
+
+/**
+ * basically web result
+ */
+data class ResultStatus(
+        /** st */
+        var st: String,
+        /** message */
+        var msg: String? = ""
+)
