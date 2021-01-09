2205c26e0a955032db7dd9fae13655c9ca8877a7
PARK Yong Seo <j@saro.me>
2021-01-09 19:25:54 +0900

    board view

--- anissia-app-external-api/src/main/kotlin/anissia/dto/BoardTopicDto.kt
+++ anissia-app-external-api/src/main/kotlin/anissia/dto/BoardTopicDto.kt
@@ -2,30 +2,25 @@ package anissia.dto
 
 import anissia.domain.BoardPost
 import anissia.domain.BoardTopic
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.LocalDateTime
 
 data class BoardTopicDto (
     var topicNo: Long = 0,
     var fixed: Boolean = false,
     var topic: String = "",
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var content: String = "",
     var postCount: Int = 0,
     var regDt: LocalDateTime = LocalDateTime.now(),
     var name: String = "",
     var posts: List<BoardPost> = listOf()
 ) {
-    constructor(boardTopic: BoardTopic): this(
+    constructor(boardTopic: BoardTopic, includeContent: Boolean = false, posts: List<BoardPost> = listOf()): this(
         topicNo = boardTopic.topicNo,
         fixed = boardTopic.fixed,
         topic = boardTopic.topic,
-        postCount = boardTopic.postCount,
-        regDt = boardTopic.regDt,
-        name = boardTopic.account?.name ?: "탈퇴회원"
-    )
-
-    constructor(boardTopic: BoardTopic, posts: List<BoardPost>): this(
-        topicNo = boardTopic.topicNo,
-        fixed = boardTopic.fixed,
-        topic = boardTopic.topic,
+        content = if (includeContent) boardTopic.content else "",
         postCount = boardTopic.postCount,
         regDt = boardTopic.regDt,
         name = boardTopic.account?.name ?: "탈퇴회원",
