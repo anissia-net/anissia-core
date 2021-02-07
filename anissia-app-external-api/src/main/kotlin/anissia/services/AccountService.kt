b897b26ede9ee61f05872d830acc9e2c65e9129b
PARK Yong Seo <j@saro.me>
2021-02-08 01:43:49 +0900

    create account service

---
+++ anissia-app-external-api/src/main/kotlin/anissia/services/AccountService.kt
@@ -0,0 +1,15 @@
+package anissia.services
+
+import anissia.configruration.logger
+import org.slf4j.Logger
+import org.springframework.stereotype.Service
+
+/**
+ *
+ */
+@Service
+class AccountService (
+) {
+    private val log: Logger = logger<AccountService>()
+
+}
