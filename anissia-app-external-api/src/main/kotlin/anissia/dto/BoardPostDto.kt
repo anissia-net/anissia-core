66fe3dadec542b42850d28638e9e84c3bcea2a22
PARK Yong Seo <j@saro.me>
2021-02-16 06:14:24 +0900

    board ready

--- anissia-app-external-api/src/main/kotlin/anissia/dto/BoardPostDto.kt
+++ anissia-app-external-api/src/main/kotlin/anissia/dto/BoardPostDto.kt
@@ -6,6 +6,7 @@ import java.time.LocalDateTime
 data class BoardPostDto (
     var postNo: Long = 0,
     var topicNo: Long = 0,
+    var root: Boolean = false,
     var content: String = "",
     var name: String = "",
     var regDt: LocalDateTime = LocalDateTime.now(),
@@ -14,6 +15,7 @@ data class BoardPostDto (
     constructor(boardPost: BoardPost): this(
         postNo = boardPost.postNo,
         topicNo = boardPost.topicNo,
+        root = boardPost.root,
         content = boardPost.content,
         name = boardPost.account?.name ?: "탈퇴회원",
         regDt = boardPost.regDt,
