fbf2344fd541f1ef1597bad46ba7ce12fbf193d0
PARK Yong Seo <j@saro.me>
2021-02-25 20:08:16 +0900

    Translator ready

--- anissia-domain-rdb/src/main/kotlin/anissia/rdb/domain/TranslatorService.kt
+++ anissia-domain-rdb/src/main/kotlin/anissia/rdb/domain/TranslatorService.kt
@@ -1,13 +1,19 @@
 package anissia.rdb.domain
 
+import anissia.rdb.repository.AgendaPollsRepository
 import anissia.rdb.repository.AgendaRepository
 import org.springframework.stereotype.Service
 
 @Service
 class TranslatorService(
-    private val agendaRepository: AgendaRepository
+    private val agendaRepository: AgendaRepository,
+    private val agendaPollsRepository: AgendaPollsRepository
 ) {
     fun getApplyList() {
+
+    }
+
+    fun getApply() {
 
     }
 
@@ -16,6 +22,14 @@ class TranslatorService(
     }
 
     fun deleteApply() {
+
+    }
+
+    fun createApplyPoll() {
+
+    }
+
+    fun deleteApplyPoll() {
 
     }
 }
