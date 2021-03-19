6e47d598a2a3b7768c8e11a6bbc81f68878f55ba
PARK Yong Seo <j@saro.me>
2021-03-19 22:36:37 +0900

    test

--- anissia-app-external-api/src/test/kotlin/anissia/dto/request/AccountRecoverPasswordRequestTest.kt
+++ anissia-app-external-api/src/test/kotlin/anissia/dto/request/AccountRecoverPasswordRequestTest.kt
@@ -1,12 +1,27 @@
 package anissia.dto.request
 
-import org.junit.jupiter.api.BeforeEach
+import me.saro.kit.Texts
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import javax.validation.Validation
+import javax.validation.Validator
 
-import org.junit.jupiter.api.Assertions.*
 
 internal class AccountRecoverPasswordRequestTest {
 
-    @BeforeEach
-    fun setUp() {
+    private var validator: Validator = Validation.buildDefaultValidatorFactory().validator
+
+    @Test
+    @DisplayName("@Valid - 계정복구(암호찾기) 암호입력")
+    fun valid() {
+
+        var tn = Texts.createRandomString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray(), 128, 512)
+        var password = "asdfaffsdf";
+
+        val sut = AccountRecoverPasswordRequest(tn, password)
+
+        val result = validator.validate(sut)
+
+        assert(result.isEmpty()) { "$sut\n\n$result" }
     }
 }
