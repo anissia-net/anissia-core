245a7561545b2cd3dffcadc672d174de41be280c
PARK Yong Seo <j@saro.me>
2021-03-21 01:25:34 +0900

    test data

--- anissia-app-external-api/src/main/kotlin/anissia/controller/TestDataController.kt
+++ anissia-app-external-api/src/main/kotlin/anissia/controller/TestDataController.kt
@@ -5,10 +5,11 @@ import anissia.configruration.logger
 import anissia.misc.As
 import anissia.rdb.domain.*
 import anissia.rdb.repository.*
+import anissia.services.ActivePanelService
+import anissia.services.AdminService
 import anissia.services.AnimeRankService
 import anissia.services.AnimeService
-import io.netty.handler.codec.http.HttpContent
-import org.apache.http.entity.ContentType
+import me.saro.kit.lang.Koreans
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.security.access.AccessDeniedException
@@ -17,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
-import javax.persistence.Column
 
 /**
  * it must use only develop server !!
@@ -32,8 +32,10 @@ class TestDataController(
     private val boardTickerRepository: BoardTickerRepository,
     private val boardTopicRepository: BoardTopicRepository,
     private val animeService: AnimeService,
+    private val adminService: AdminService,
     private val animeRepository: AnimeRepository,
     private val passwordEncoder: PasswordEncoder,
+    private val activePanelService: ActivePanelService,
     @Value("\${env}") private val env: String
 ) {
     var log = logger<AnissiaCoreApplication>()
@@ -62,20 +64,8 @@ class TestDataController(
 
         var list = StringBuilder(1024)
 
-        //INSERT INTO anissia.account (an, ban_expire_dt, email, last_login_dt, name, old_account, old_account_no, password, reg_dt) VALUES (1, null, 'j@saro.me', '2021-02-09 09:00:56', '박용서', 'saro', 2, '{bcrypt}$2a$10$mRmuhIlTgx2/54SkMpEJb..nNiwC1CABakh5pNx8.xVFeEMzrOIJG', '2010-08-18 00:00:00');
-
-        accountRepository.save(Account(
-            email = "admin@test.com",
-            password = passwordEncoder.encode("asdfasdf"),
-            name = "어드민",
-            roles = mutableSetOf(AccountRole.ROOT, AccountRole.TRANSLATOR)
-        ))
-
-        accountRepository.save(Account(
-            email = "user@test.com",
-            password = passwordEncoder.encode("asdfasdf"),
-            name = "유저"
-        ))
+        accountRepository.save(Account(email = "admin@test.com", password = passwordEncoder.encode("asdfasdf"), name = "어드민", roles = mutableSetOf(AccountRole.ROOT, AccountRole.TRANSLATOR)))
+        accountRepository.save(Account(email = "user@test.com", password = passwordEncoder.encode("asdfasdf"), name = "유저"))
 
         // board ticker
         boardTickerRepository.save(BoardTicker("notice", "공지사항", "ROOT,TRANSLATOR", ""))
@@ -83,11 +73,35 @@ class TestDataController(
         list.appendLine("기본 게시판 정보 삽입")
 
         // anime genre
-        animeGenreRepository.saveAll(listOf("SF","공포","드라마","로맨스","모험","무협","스포츠","액션","음악","코미디","판타지","호러").map { AnimeGenre(it) })
+        val genres = listOf("SF","공포","드라마","로맨스","모험","무협","스포츠","액션","음악","코미디","판타지","호러")
+        animeGenreRepository.saveAll(genres.map { AnimeGenre(it) })
         list.appendLine("기본 장르 삽입")
 
-        boardTopicRepository.save(BoardTopic(ticker = "notice", topic = "공지사항", an = 1))
-        boardTopicRepository.save(BoardTopic(ticker = "inquiry", topic = "문의입니다.", an = 2))
+        for (i in 1..5) {
+            boardTopicRepository.save(BoardTopic(ticker = "notice", topic = "공지사항 $i", an = 1))
+            boardTopicRepository.save(BoardTopic(ticker = "inquiry", topic = "문의입니다 $i.", an = 2))
+            activePanelService.saveText("운영게시물 $i", true, 1)
+        }
+
+        listOf("일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "외전", "기타").forEachIndexed { wi, week ->
+            for (i in 1..10) {
+                val subject = "$week 애니메이션 $i"
+                animeRepository.save(Anime(
+                    week = "$wi",
+                    time = "${"%02d".format((Math.random() * 23).toInt())}:${"%02d".format((Math.random() * 59).toInt())}",
+                    subject = subject,
+                    autocorrect = Koreans.toJasoAtom(subject),
+                    genres = genres[(genres.size * Math.random()).toInt()],
+                    startDate = "",
+                    endDate = "",
+                    website = "",
+                ))
+            }
+        }
+
+        rank()
+
+        allAnime()
 
         return list.appendLine("완료").toString()
     }
@@ -97,7 +111,7 @@ class TestDataController(
     fun rank(): String {
         checkDevelopServer()
 
-        for (animeNo in 1..2000) {
+        for (animeNo in 1..100) {
             if ((Math.random() * 10).toInt() != 0) {
                 continue
             }
