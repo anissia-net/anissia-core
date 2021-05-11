99caed923e11e6e32f0c310d6a4549298095db39
PARK Yong Seo <j@saro.me>
2021-05-12 02:26:36 +0900

    gradle: kotlin, spring version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "1.4.20"
+	val kotlinVersion = "1.5.0"
 	kotlin("jvm") version kotlinVersion apply false
 	kotlin("kapt") version kotlinVersion apply false
 	kotlin("plugin.spring") version kotlinVersion apply false
 	kotlin("plugin.jpa") version kotlinVersion apply false
 	kotlin("plugin.allopen") version kotlinVersion apply false
-	id("org.springframework.boot") version "2.4.0" apply false
-	id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
+	id("org.springframework.boot") version "2.4.5" apply false
+	id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
 }
 
 allprojects {
