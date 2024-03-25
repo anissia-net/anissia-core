76d751a4d5d84913973506e72d0168eccd92570c
PARK Yong Seo <j@saro.me>
2024-03-26 01:49:41 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "2.0.0-Beta4"
+	val kotlinVersion = "2.0.0-Beta5"
 	id("org.jetbrains.kotlin.jvm") version kotlinVersion
 	id("org.jetbrains.kotlin.kapt") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-	id("org.springframework.boot") version "3.2.3"
+	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
-	id("org.ec4j.editorconfig") version "0.0.3"
+	id("org.ec4j.editorconfig") version "0.1.0"
 	id("idea")
 }
 
