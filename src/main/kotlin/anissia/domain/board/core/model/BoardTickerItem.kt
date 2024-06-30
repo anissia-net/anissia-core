fd2414d01b3a1dd20c438c3f3d5fb151a49162f8
PARK Yong Seo <j@saro.me>
2024-06-30 17:51:28 +0900

    문의 게시판 개선 - 알림문구.

--- src/main/kotlin/anissia/domain/board/core/model/BoardTickerItem.kt
+++ src/main/kotlin/anissia/domain/board/core/model/BoardTickerItem.kt
@@ -7,11 +7,13 @@ class BoardTickerItem (
     val name: String = "",
     val writeTopicRoles: List<String> = listOf(),
     val writePostRoles: List<String> = listOf(),
+    val placeholder: String = "내용",
 ) {
     constructor(boardTicker: BoardTicker): this(
         ticker = boardTicker.ticker,
         name = boardTicker.name,
         writeTopicRoles = boardTicker.writeTopicRoles.run { split(",".toRegex()) } .filter { it != "" },
-        writePostRoles = boardTicker.writePostRoles.run { split(",".toRegex()) } .filter { it != "" }
+        writePostRoles = boardTicker.writePostRoles.run { split(",".toRegex()) } .filter { it != "" },
+        placeholder = boardTicker.placeholder
     )
 }
