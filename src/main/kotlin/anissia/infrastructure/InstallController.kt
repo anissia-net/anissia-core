7f07dd0a286abed86bed7d13e9b6b53ac3f60d18
PARK Yong Seo <j@saro.me>
2024-07-03 22:18:50 +0900

    설치 컨트롤러 준비

--- src/main/kotlin/anissia/infrastructure/InstallController.kt
+++ src/main/kotlin/anissia/infrastructure/InstallController.kt
@@ -1,16 +1,21 @@
 package anissia.infrastructure
 
 import org.springframework.context.annotation.Profile
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
+/**
+ * 이 컨트롤러는 개발환경에서만 사용할 수 있다.
+ * 설치 전용 컨트롤러로 컨트롤러에 로직이 포함된다.
+ */
 @Profile("dev", "local")
-@RestController("/dev")
-class DevController(
+@RestController
+class InstallController(
 
 ) {
-
+    @Transactional
     @GetMapping("/install")
     fun install(): String {
 
