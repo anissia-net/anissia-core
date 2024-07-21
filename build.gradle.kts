df0a58b50e24650936f14d4927b042f24ee20e2e
PARK Yong Seo <j@saro.me>
2024-07-21 22:16:43 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
 	id("org.ec4j.editorconfig") version "0.1.0"
 	id("idea")
 }
