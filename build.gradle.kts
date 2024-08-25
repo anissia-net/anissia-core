94e2912b475d9c4c54d47a07be2b3c846eb71297
PARK Yong Seo <j@saro.me>
2024-08-25 18:21:21 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-	val kotlinVersion = "2.0.0"
+	val kotlinVersion = "2.0.20"
 	id("org.jetbrains.kotlin.jvm") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.ec4j.editorconfig") version "0.1.0"
 	id("idea")
@@ -26,7 +26,6 @@ idea {
 dependencies {
 	// JPA query dsl
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 
 	// DB connector
@@ -36,7 +35,7 @@ dependencies {
 	// elasticsearch
 	// - https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/current/getting-started-java.html
 	//implementation("co.elastic.clients:elasticsearch-java:8.14.1") // 일반
-	implementation("org.elasticsearch.client:elasticsearch-rest-client:8.14.1") // 로우레벨 (결정)
+	implementation("org.elasticsearch.client:elasticsearch-rest-client:8.15.0") // 로우레벨 (결정)
 
 	// lib
 	implementation("me.saro:kit:0.2.0")
