c83815633e929ff0f5fc40c2f1cab5f52f6a6deb
marker <j@saro.me>
2025-02-09 23:18:38 +0900

    Utils

--- src/main/kotlin/anissia/infrastructure/service/EmailService.kt
+++ src/main/kotlin/anissia/infrastructure/service/EmailService.kt
@@ -1,7 +1,6 @@
 package anissia.infrastructure.service
 
-import anissia.infrastructure.common.As
-import anissia.shared.ResultWrapper
+import anissia.infrastructure.common.logger
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.mail.Message
@@ -10,6 +9,7 @@ import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import java.io.File
 import java.util.*
 
@@ -45,7 +45,7 @@ class EmailService (
     private val enable: Boolean = configFile.exists()
     private val props: Map<String, String> = if (enable) ObjectMapper().readValue(configFile, object: TypeReference<Map<String, String>>(){}) else mapOf()
     private val sender: JavaMailSenderImpl = JavaMailSenderImpl()
-    private val log: Logger = As.logger<EmailService>()
+    private val log: Logger = logger<EmailService>()
 
     init {
         if (enable) {
@@ -76,7 +76,8 @@ class EmailService (
      * @param subject mail subject
      * @param htmlContent mail content (html)
      */
-    fun send(to: String, subject: String, htmlContent: String): ResultWrapper<Unit> = send(listOf(to), listOf(), subject, htmlContent)
+    fun send(to: String, subject: String, htmlContent: String): Mono<Void> =
+        send(listOf(to), listOf(), subject, htmlContent)
 
     /**
      * @param to receive to
@@ -84,36 +85,23 @@ class EmailService (
      * @param subject mail subject
      * @param htmlContent mail content (html)
      */
-    fun send(to: List<String>, cc: List<String>, subject: String, htmlContent: String): ResultWrapper<Unit> = try {
-        if (enable) {
-            sender.send {
-                // from / to / cc / subject / text
-                it.setFrom(InternetAddress(props["from"]))
-                to.forEach { to -> it.addRecipient(Message.RecipientType.TO, InternetAddress(to)) }
-                cc.forEach { cc -> it.addRecipient(Message.RecipientType.CC, InternetAddress(cc)) }
-                it.subject = subject
-                it.setText(htmlContent, "UTF-8", "html")
+    fun send(to: List<String>, cc: List<String>, subject: String, htmlContent: String): Mono<Void> =
+        Mono.fromCallable<Void> {
+            if (enable) {
+                sender.send {
+                    // from / to / cc / subject / text
+                    it.setFrom(InternetAddress(props["from"]))
+                    to.forEach { to -> it.addRecipient(Message.RecipientType.TO, InternetAddress(to)) }
+                    cc.forEach { cc -> it.addRecipient(Message.RecipientType.CC, InternetAddress(cc)) }
+                    it.subject = subject
+                    it.setText(htmlContent, "UTF-8", "html")
+                }
+            } else {
+                log.info("EMAIL DEVELOP MODE $to -> $cc -> $subject\n$htmlContent")
             }
-            ResultWrapper.ok()
-        } else {
-            log.debug("""
-                    EMAIL DEVELOP MODE
-                    to: $to
-                    cc: $cc
-                    subject: $subject
-                    content: $htmlContent
-                """.trimIndent())
-            ResultWrapper.ok()
+            null
+        }.doOnError {
+            log.info("EMAIL ERROR $to -> $cc -> $subject\n$htmlContent")
         }
-    } catch (e: Exception) {
-        log.debug("""
-            EMAIL ERROR
-            to: $to
-            cc: $cc
-            subject: $subject
-            content: $htmlContent
-        """.trimIndent())
-        log.error(e.message, e)
-        ResultWrapper.error(e.message)
-    }
+
 }
--- src/main/kotlin/anissia/infrastructure/service/GoogleAnalyticsProxyService.kt
+++ src/main/kotlin/anissia/infrastructure/service/GoogleAnalyticsProxyService.kt
@@ -1,26 +1,30 @@
 package anissia.infrastructure.service
 
-import anissia.infrastructure.common.As.Companion.encodeUrl
-import anissia.infrastructure.common.As.Companion.getHttp400
+import anissia.infrastructure.common.encodeUrl
+import anissia.infrastructure.common.getHttp400
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.server.ServerWebExchange
-
+import reactor.core.publisher.Mono
 
 @Service
 class GoogleAnalyticsProxyService(
-    private val asyncService: AsyncService,
-    private val restTemplate: RestTemplate,
     @Value("\${google.analytics.id}") private val id: String
 ) {
-    fun send(path: String, exchange: ServerWebExchange) {
-        val ip = exchange.request.remoteAddress?.address?.hostAddress!!
-        val ua = exchange.request.headers["user-agent"]?.firstOrNull() ?: throw getHttp400("does not exist user-agent")
+    private val apiClient = WebClient.builder().baseUrl("https://www.google-analytics.com/collect").build()
 
-        asyncService.async {
-            val request = "v=1&tid=$id&cid=$ip&t=pageview&dp=${path.encodeUrl()}&uip=$ip&ua=${ua.encodeUrl()}"
-            restTemplate.postForObject("https://www.google-analytics.com/collect", request, ByteArray::class.java)
-        }
-    }
+    fun send(path: String, exchange: ServerWebExchange): Mono<Any> =
+        Mono.just(exchange.request)
+            .flatMap { request ->
+                val ip = request.remoteAddress?.address?.hostAddress!!
+                val ua = request.headers["user-agent"]
+                    ?.firstOrNull()
+                    ?.encodeUrl
+                    ?: return@flatMap Mono.error<Unit>(getHttp400("does not exist user-agent"))
+
+                apiClient.post()
+                    .bodyValue("v=1&tid=$id&cid=$ip&t=pageview&dp=${path.encodeUrl}&uip=$ip&ua=$ua")
+                    .exchangeToMono { Mono.just(it.statusCode().is2xxSuccessful) }
+            }
 }
