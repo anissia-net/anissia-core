6d271badc597a5cacec8bc10df9465ca45dc9d0a
marker <j@saro.me>
2025-02-09 23:18:34 +0900

    Utils

--- src/main/kotlin/anissia/infrastructure/service/ElasticsearchService.kt
+++ src/main/kotlin/anissia/infrastructure/service/ElasticsearchService.kt
@@ -1,54 +1,50 @@
 package anissia.infrastructure.service
 
-import org.apache.http.HttpHost
-import org.apache.http.auth.AuthScope
-import org.apache.http.auth.UsernamePasswordCredentials
-import org.apache.http.impl.client.BasicCredentialsProvider
-import org.elasticsearch.client.Request
-import org.elasticsearch.client.Response
-import org.elasticsearch.client.RestClient
+import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.*
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Service
 class ElasticsearchService(
-    @Value("\${anissia.ndb.host}")
-    private val host: String,
-    @Value("\${anissia.ndb.port}")
-    private val port: Int,
+    @Value("\${anissia.ndb.url}")
+    private val url: String,
     @Value("\${anissia.ndb.username}")
     private val username: String,
     @Value("\${anissia.ndb.password}")
     private val password: String,
 ) {
-    private val credentialsProvider = BasicCredentialsProvider()
-        .apply { setCredentials(AuthScope.ANY, UsernamePasswordCredentials(username, password)) }
-
-    fun open(): RestClient = RestClient
-        .builder(HttpHost(host, port))
-        .setHttpClientConfigCallback { it.setDefaultCredentialsProvider(credentialsProvider) }
+    private val elasticClient = WebClient.builder()
+        .baseUrl(url)
+        .defaultHeaders { if ((username + password).isNotBlank()) { it.setBasicAuth(username, password) } }
         .build()
 
-    fun request(method: String, endpoint: String, body: String? = null): Response = open().use {
-        val req = Request(method, endpoint)
-        if (body != null) {
-            req.setJsonEntity(body)
-        }
-        it.performRequest(req)
-    }
+    fun requestRaw(method: HttpMethod, uri: String, body: String? = null): WebClient.RequestBodySpec =
+        elasticClient.method(method)
+            .uri(uri).apply { if (body != null) bodyValue(body) }
 
-    fun existsIndex(index: String): Boolean =
-        request("HEAD", "/$index").statusLine.statusCode == 200
+    fun requestStateOk(method: HttpMethod, uri: String, body: String? = null): Mono<Boolean> =
+        requestRaw(method, uri, body)
+            .exchangeToMono { Mono.just(it.statusCode().is2xxSuccessful) }
 
-    fun deleteIndex(index: String): Boolean =
-        request("DELETE", "/$index").statusLine.statusCode == 200
+    fun request(method: HttpMethod, uri: String, body: String? = null): Mono<JsonNode> =
+        requestRaw(method, uri, body).retrieve().bodyToMono(JsonNode::class.java)
 
-    fun deleteIndexIfExists(index: String): Boolean =
-        if (existsIndex(index)) deleteIndex(index) else false
+    fun existsIndex(index: String): Mono<Boolean> =
+        requestStateOk(HEAD, "/$index")
 
-    fun createIndex(index: String, body: String): Boolean =
-        request("PUT", "/$index", body).statusLine.statusCode == 200
+    fun deleteIndex(index: String): Mono<Boolean> =
+        requestStateOk(DELETE, "/$index")
 
-    fun updateIndex(forceCreate: Boolean, index: String, body: String): Boolean =
-        request("PUT", "/$index/_mapping", body).statusLine.statusCode == 200
+    fun deleteIndexIfExists(index: String): Mono<Boolean> =
+        existsIndex(index).flatMap { if (it) deleteIndex(index) else Mono.just(false) }
+
+    fun createIndex(index: String, body: String): Mono<Boolean> =
+        requestStateOk(PUT, "/$index", body)
+
+    fun updateIndex(forceCreate: Boolean, index: String, body: String): Mono<Boolean> =
+        requestStateOk(PUT, "/$index/_mapping", body)
 }
