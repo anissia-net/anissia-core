baeb2ea645ca76b009264017cdaf41c12a54cfe0
marker <j@saro.me>
2025-02-24 20:40:10 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	val kotlinVersion = "2.1.20-Beta1"
+	val kotlinVersion = "2.1.0"
 	id("org.jetbrains.kotlin.jvm") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-	id("org.springframework.boot") version "3.4.1"
+	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.ec4j.editorconfig") version "0.1.0"
 	id("idea")
@@ -36,7 +36,7 @@ dependencies {
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
 	// elasticsearch
-	implementation("org.elasticsearch.client:elasticsearch-rest-client:8.17.0")
+	implementation("org.elasticsearch.client:elasticsearch-rest-client:8.17.2")
 
 	// lib
 	implementation("me.saro:kit:0.2.3")
